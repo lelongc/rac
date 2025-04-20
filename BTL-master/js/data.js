882129d6eefac8b1ce7d1ea4f501a02365404eb9
Shima_Rin <lelong190110gmail.com>
2025-04-20 22:59:31 +0700

    Update image paths in data.js for correct directory structure

--- BTL-master/js/data.js
+++ BTL-master/js/data.js
@@ -1,255 +1,254 @@
 let data = [
   {
-    img: "./Images/lọ hoa5/lothuytinh.jpg",
+    img: "../Images/lọ hoa5/lothuytinh.jpg",
     title: "Tủ Đồ Đựng Giày",
   },
   {
-    img: "./Images/lọ hoa5/lothuytinhtrangtri.jpg",
+    img: "../Images/lọ hoa5/lothuytinhtrangtri.jpg",
     title: "Tủ Đồ Kiểu Tân Cổ Điển",
   },
   {
-    img: "./Images/trang trí để bàn6/hoamattroi.jpg",
+    img: "../Images/trang trí để bàn6/hoamattroi.jpg",
     title: "Bàn làm việc chữ L",
   },
   {
-    img: "./Images/trang trí để bàn6/hopkhangiaycaocap.jpg",
+    img: "../Images/trang trí để bàn6/hopkhangiaycaocap.jpg",
     title: "Bàn làm việc chữ U",
   },
   {
-    img: "./Images/trang trí để bàn6/khaytrangtrihoacaocap.png",
+    img: "../Images/trang trí để bàn6/khaytrangtrihoacaocap.png",
     title: "Bàn làm việc đứng",
   },
   {
-    img: "./Images/trang trí để bàn6/nuthanlongvu.jpg",
+    img: "../Images/trang trí để bàn6/nuthanlongvu.jpg",
     title: "Bàn làm việc có hộc tủ",
   },
   {
-    img: "./Images/đèn trang trí1/DenMikeyTrangTri.jpg",
+    img: "../Images/đèn trang trí1/DenMikeyTrangTri.jpg",
     title: "Đèn Mikey trang trí",
   },
   {
-    img: "./Images/đồng hồ2/donghotreotuongphihanhgia.jpg",
+    img: "../Images/đồng hồ2/donghotreotuongphihanhgia.jpg",
     title: "Kệ Tivi Góc",
   },
   {
-    img: "./Images/đèn trang trí1/Den-ban-trang-triLL.jpg",
+    img: "../Images/đèn trang trí1/Den-ban-trang-triLL.jpg",
     title: "Đèn bàn trang trí",
   },
   {
-    img: "./Images/đèn trang trí1/DenTrangTri.jpg",
+    img: "../Images/đèn trang trí1/DenTrangTri.jpg",
     title: "Đèn trang trí",
   },
   {
-    img: "./Images/đồng hồ2/donghotreotuong.jpg",
+    img: "../Images/đồng hồ2/donghotreotuong.jpg",
     title: "Kệ Tivi Treo Tường",
   },
   {
-    img: "./Images/đồng hồ2/DongHoQuaLac.jpg",
+    img: "../Images/đồng hồ2/DongHoQuaLac.jpg",
     title: "Kệ Tivi Tủ Ngăn Kéo",
   },
   {
-    img: "./Images/đồng hồ2/donghotoigianbacau.jpg",
+    img: "../Images/đồng hồ2/donghotoigianbacau.jpg",
     title: "Kệ Tivi Dưới Đất",
   },
   {
-    img: "./Images/đồng hồ2/DongHoTRangTri.jpg",
+    img: "../Images/đồng hồ2/DongHoTRangTri.jpg",
     title: "Kệ Tivi Mặt Kính",
   },
   {
-    img: "./Images/đồng hồ2/donghotreotuongqualacconchim.jpg",
+    img: "../Images/đồng hồ2/donghotreotuongqualacconchim.jpg",
     title: "Kệ Tivi Treo Tường  con chim",
   },
   {
-    img: "./Images/đèn trang trí1/Banana Lightning.jpg",
+    img: "../Images/đèn trang trí1/Banana Lightning.jpg",
     title: "Đèn quả chuối",
   },
   {
-    img: "./Images/đồng hồ2/DonghotoigianbacAuqualac.jpg",
+    img: "../Images/đồng hồ2/DonghotoigianbacAuqualac.jpg",
     title: "Kệ Tivi Từ Gỗ Tự Nhiên",
   },
   {
-    img: "./Images/đồng hồ2/donghotreotuong2.jpg",
+    img: "../Images/đồng hồ2/donghotreotuong2.jpg",
     title: "Kệ Tivi Treo Tường",
   },
   {
-    img: "./Images/đèn trang trí1/DenTrangTriMatTrang.jpg",
+    img: "../Images/đèn trang trí1/DenTrangTriMatTrang.jpg",
     title: "Đèn trang trí mặt trăng",
   },
   {
-    img: "./Images/đèn trang trí1/LionLightning.jpg",
+    img: "../Images/đèn trang trí1/LionLightning.jpg",
     title: "Đèn trang trí LionLightning",
   },
   {
-    img: "./Images/đèn trang trí1/Den-ban-trang-triLLT.jpg",
+    img: "../Images/đèn trang trí1/Den-ban-trang-triLLT.jpg",
     title: "Đèn bàn trang trí",
   },
   {
-    img: "./Images/đèn trang trí1/Den-ban-trang-triLLTB19.jpg",
+    img: "../Images/đèn trang trí1/Den-ban-trang-triLLTB19.jpg",
     title: "Đèn bàn trang trí",
   },
   {
-    img: "./Images/đèn trang trí1/DenBanTrangTri.jpg",
+    img: "../Images/đèn trang trí1/DenBanTrangTri.jpg",
     title: "Đèn bàn trang trí",
   },
   {
-    img: "./Images/đèn trang trí1/DenBanTRangTri2.jpg",
+    img: "../Images/đèn trang trí1/DenBanTRangTri2.jpg",
     title: "Đèn bàn trang trí",
   },
 
   {
-    img: "./Images/khác4/bantranho2.jpg",
+    img: "../Images/khác4/bantranho2.jpg",
     title: "Bàn trà nhỏ",
   },
   {
-    img: "./Images/đồng hồ2/donghochimcong.jpg",
+    img: "../Images/đồng hồ2/donghochimcong.jpg",
     title: "Kệ Tivi Chân Kim Loại",
   },
   {
-    img: "./Images/đồng hồ2/donghotrangtri3.jpg",
+    img: "../Images/đồng hồ2/donghotrangtri3.jpg",
     title: "Kệ Tivi Mặt Kính",
   },
   {
-    img: "./Images/đồng hồ2/DHTT1.jpg",
+    img: "../Images/đồng hồ2/DHTT1.jpg",
     title: "Kệ Tivi Treo Tường",
   },
   {
-    img: "./Images/đồng hồ2/DHTT2.jpg",
+    img: "../Images/đồng hồ2/DHTT2.jpg",
     title: "Kệ Tivi Hòa Hợp Với Tường",
   },
   {
-    img: "./Images/đồng hồ2/DHTT3.jpg",
+    img: "../Images/đồng hồ2/DHTT3.jpg",
     title: "Kệ Tivi Với Kệ Bên",
   },
   {
-    img: "./Images/khác4/boghesophabingo.jpg",
+    img: "../Images/khác4/boghesophabingo.jpg",
     title: "Bộ ghế sopha bí ngô",
   },
   {
-    img: "./Images/khác4/ghethubong.jpg",
+    img: "../Images/khác4/ghethubong.jpg",
     title: "Ghế thú bông",
   },
   {
-    img: "./Images/khác4/keconsoleversace.jpg",
+    img: "../Images/khác4/keconsoleversace.jpg",
     title: "Kệ Console Versace",
   },
 
-  
   {
-    img: "./Images/khác4/bantradammay.jpg",
+    img: "../Images/khác4/bantradammay.jpg",
     title: "Bàn trà đám mây",
   },
   {
-    img: "./Images/khác4/bantranho.jpg",
+    img: "../Images/khác4/bantranho.jpg",
     title: "Bàn trà nhỏ",
   },
   {
-    img: "./Images/lọ hoa5/boHoalanvang.jpg",
+    img: "../Images/lọ hoa5/boHoalanvang.jpg",
     title: "Tủ Đồ Mặt Kính Cường Lực",
   },
   {
-    img: "./Images/lọ hoa5/bolohoala.jpg",
+    img: "../Images/lọ hoa5/bolohoala.jpg",
     title: "Tủ Đồ Vách Ngăn",
   },
   {
-    img: "./Images/lọ hoa5/bolohoatrangtri.jpg",
+    img: "../Images/lọ hoa5/bolohoatrangtri.jpg",
     title: "Tủ Đồ Thông Minh",
   },
   {
-    img: "./Images/khác4/keconsole.jpg",
+    img: "../Images/khác4/keconsole.jpg",
     title: "Kệ Console",
   },
   {
-    img: "./Images/khác4/keconsole2.jpg",
+    img: "../Images/khác4/keconsole2.jpg",
     title: "Kệ Console",
   },
   {
-    img: "./Images/lọ hoa5/bolohoaxanh.jpg",
+    img: "../Images/lọ hoa5/bolohoaxanh.jpg",
     title: "Tủ Đồ Cao Cấp",
   },
   {
-    img: "./Images/lọ hoa5/canhhongtrangtri.jpg",
+    img: "../Images/lọ hoa5/canhhongtrangtri.jpg",
     title: "Tủ Đồ Gỗ Sồi",
   },
 
   {
-    img: "./Images/treo tường7/Tranh-Su-TuLDT115lingdecor1-300x300.jpg",
+    img: "../Images/treo tường7/Tranh-Su-TuLDT115lingdecor1-300x300.jpg",
     title: "Kệ Treo Tường Đơn Giản",
   },
   {
-    img: "./Images/treo tường7/Tranh-treo-tuong-phi-hanh-gia-ngoiLDT80lingdecor2-300x300.png",
+    img: "../Images/treo tường7/Tranh-treo-tuong-phi-hanh-gia-ngoiLDT80lingdecor2-300x300.png",
     title: "Kệ Treo Tường Tân Cổ Điển",
   },
 
   {
-    img: "./Images/trang trí để bàn6/trangtrideban.jpg",
+    img: "../Images/trang trí để bàn6/trangtrideban.jpg",
     title: "Bàn làm việc treo tường",
   },
   {
-    img: "./Images/trang trí để bàn6/vongtronduquay.jpg",
+    img: "../Images/trang trí để bàn6/vongtronduquay.jpg",
     title: "Bàn làm việc gấp gọn",
   },
   {
-    img: "./Images/trang trí để bàn6/phihanhgiadokhayden.jpg",
+    img: "../Images/trang trí để bàn6/phihanhgiadokhayden.jpg",
     title: "Bàn làm việc có kệ sách tích hợp",
   },
   {
-    img: "./Images/trang trí để bàn6/phihanhgiagaukemhoa.jpg",
+    img: "../Images/trang trí để bàn6/phihanhgiagaukemhoa.jpg",
     title: "Bàn làm việc cụm ",
   },
   {
-    img: "./Images/trang trí để bàn6/thokemloablutooth.jpg",
+    img: "../Images/trang trí để bàn6/thokemloablutooth.jpg",
     title: "Bàn làm việc gaming",
   },
   {
-    img: "./Images/treo tường7/Decor-treo-tuong-giot-nuoc-cao-capLDT120lingdecor1-300x300.jpg",
+    img: "../Images/treo tường7/Decor-treo-tuong-giot-nuoc-cao-capLDT120lingdecor1-300x300.jpg",
     title: "Kệ Treo Tường Mặt Trăng",
   },
   {
-    img: "./Images/treo tường7/Decor-treo-tuongLDT105lingdecor2-300x300.jpg",
+    img: "../Images/treo tường7/Decor-treo-tuongLDT105lingdecor2-300x300.jpg",
     title: "Kệ Treo Tường Treo Chậu Cây",
   },
   {
-    img: "./Images/treo tường7/O1CN01ihT8z31PzHTSVjLZn_1761941911-300x300.jpg",
+    img: "../Images/treo tường7/O1CN01ihT8z31PzHTSVjLZn_1761941911-300x300.jpg",
     title: "Kệ Treo Tường Gỗ Sồi",
   },
   {
-    img: "./Images/treo tường7/trangKhinhKhiCauTreotuong.jpg",
+    img: "../Images/treo tường7/trangKhinhKhiCauTreotuong.jpg",
     title: "Kệ Treo Tường Hình Lục Giác",
   },
   {
-    img: "./Images/treo tường7/Tranh-Gau-trai-timLDT113lingdecor2-300x300.jpg",
+    img: "../Images/treo tường7/Tranh-Gau-trai-timLDT113lingdecor2-300x300.jpg",
     title: "Kệ Treo Tường Nghệ Thuật",
   },
   {
-    img: "./Images/tượng đỡ khay trang trí8/Gau-hong-be-khay-trang-triLDB270lingdecor2-300x300.jpg",
+    img: "../Images/tượng đỡ khay trang trí8/Gau-hong-be-khay-trang-triLDB270lingdecor2-300x300.jpg",
     title: "Sofa hiện đại ",
   },
   {
-    img: "./Images/tượng đỡ khay trang trí8/gaucamtraitim.png",
+    img: "../Images/tượng đỡ khay trang trí8/gaucamtraitim.png",
     title: "Sofa tân cổ điển",
   },
   {
-    img: "./Images/treo tường7/Tranh-treo-tuong-Ton-Ngo-KhongLDT117lingdecor2-300x300.jpg",
+    img: "../Images/treo tường7/Tranh-treo-tuong-Ton-Ngo-KhongLDT117lingdecor2-300x300.jpg",
     title: "Kệ Treo Tường Mặt Kính",
   },
   {
-    img: "./Images/tượng đỡ khay trang trí8/Bulldog-do-khayLDB184lingdecor-300x300.jpg",
+    img: "../Images/tượng đỡ khay trang trí8/Bulldog-do-khayLDB184lingdecor-300x300.jpg",
     title: "Sofa đơn",
   },
   {
-    img: "./Images/tượng đỡ khay trang trí8/Tho-trang-triLDB269lingdecor1-300x300.jpg",
+    img: "../Images/tượng đỡ khay trang trí8/Tho-trang-triLDB269lingdecor1-300x300.jpg",
     title: "Sofa băng",
   },
   {
-    img: "./Images/tượng đỡ khay trang trí8/phihanhgiadokhaykemden.jpg",
+    img: "../Images/tượng đỡ khay trang trí8/phihanhgiadokhaykemden.jpg",
     title: "Sofa giường",
   },
   {
-    img: "./Images/tượng đỡ khay trang trí8/Kim-Nguu-be-doLDB183lingdecor1-300x300.jpg",
+    img: "../Images/tượng đỡ khay trang trí8/Kim-Nguu-be-doLDB183lingdecor1-300x300.jpg",
     title: "Sofa chữ U",
   },
   {
-    img: "./Images/tượng đỡ khay trang trí8/Phi-hanh-gia-be-khay-mat-guongLDB255lingdecor5-300x300.jpg",
+    img: "../Images/tượng đỡ khay trang trí8/Phi-hanh-gia-be-khay-mat-guongLDB255lingdecor5-300x300.jpg",
     title: "Sofa chữ L",
   },
 ];
