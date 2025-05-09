821159120a9a84b265813a60ecf9e8294e4a78e7
Shima_Rin <lelong190110gmail.com>
2025-05-09 14:44:08 +0700

    cuoi16cuoi16

--- ck/test copy/js/main.js
+++ ck/test copy/js/main.js
@@ -3,25 +3,30 @@ $(document).ready(function () {
 
   // Setup image preview functionality
   function setupImagePreviews() {
-    $('input[type="file"][accept="image/*"]').each(function() {
-      const inputId = $(this).attr('id');
-      const previewId = inputId + '_preview';
-      
-      $(this).on('change', function() {
+    $('input[type="file"][accept="image/*"]').each(function () {
+      const inputId = $(this).attr("id");
+      const previewId = inputId + "_preview";
+
+      $(this).on("change", function () {
         const file = this.files[0];
         if (file) {
           const reader = new FileReader();
-          reader.onload = function(e) {
-            $('#' + previewId).attr('src', e.target.result).show();
-          }
+          reader.onload = function (e) {
+            // Store the image data but don't display it
+            $("#" + previewId)
+              .attr("src", e.target.result)
+              .hide(); // Keep hiding the preview
+          };
           reader.readAsDataURL(file);
         } else {
-          $('#' + previewId).attr('src', '').hide();
+          $("#" + previewId)
+            .attr("src", "")
+            .hide();
         }
       });
     });
   }
-  
+
   setupImagePreviews();
 
   function setupReadonlyFields() {
@@ -31,76 +36,39 @@ $(document).ready(function () {
     function update_readonly_display() {
       const selectedValue = $source_select.val();
       if (selectedValue) {
-       $target_readonly_display.val($source_select.find("option:selected").text());
+        $target_readonly_display.val(selectedValue);
       } else {
-          $target_readonly_display.val("");
+        $target_readonly_display.val("");
       }
     }
-    
+
     $source_select.on("change", update_readonly_display);
     update_readonly_display();
   }
 
   setupReadonlyFields();
 
-  $("#generatedForm").on("submit", function(event) {
+  $("#generatedForm").on("submit", function (event) {
     event.preventDefault();
-    
+
     let isValid = true;
-    const dateInput = $("#date");
-    const text_inputInput = $("#text_input");
-    const text_inputPattern = /^[A-Z][a-z]*(\s+[A-Z][a-z]*)+$/;
 
     $(".form-control").removeClass("is-invalid is-valid");
     $(".invalid-feedback").hide();
 
-    if (dateInput.val() !== "") {
-      {
-      const selectedDate = new Date(dateInput.val());
-      const today = new Date(); today.setHours(0,0,0,0);
-      let dateIsValid = true;
-      let errorMessage = "";
-      
-      if (selectedDate >= today) {
-        dateIsValid = false;
-        errorMessage = "ỷyery";
-      }
-      
-      if (!dateIsValid) {
-        dateInput.addClass("is-invalid");
-        dateInput.siblings(".invalid-feedback").text(errorMessage).show();
-        isValid = false;
-      } else {
-        dateInput.addClass("is-valid");
-      }
-    }
-    }
-
-    if (text_inputInput.val().trim() !== "") {
-      if (!text_inputPattern.test(text_inputInput.val().trim())) {
-      text_inputInput.addClass("is-invalid");
-      text_inputInput.siblings(".invalid-feedback").text("hghhfghfh").show();
-      isValid = false;
-    } else {
-      text_inputInput.addClass("is-valid");
-    }
-    }
-
     if (isValid) {
       const rowData = {
-        h: $("#image_upload_preview").attr("src") || "",
-        hh: $("#select").val().trim() || "",
-        jj: $("#date").val().trim() || "",
-        jgjgj: $("#text_input").val().trim() || ""
+        ma: $("#select").val().trim() || "",
+        mail: $("#image_upload_preview").attr("src") || "",
       };
 
       addRowToTable(rowData);
-      
+
       this.reset();
       $(".form-control").removeClass("is-valid is-invalid");
       // Reset image previews
       $('img[id$="_preview"]').attr("src", "").hide();
-      
+
       const modalElement = document.getElementById("myModal");
       if (modalElement) {
         const modalInstance = bootstrap.Modal.getInstance(modalElement);
@@ -113,45 +81,26 @@ $(document).ready(function () {
     }
   });
 
-function addRowToTable(rowData) {
-  const $tableBody = $("#dataTableBody");
-  const $newRow = $("<tr></tr>");
-  
-  $newRow.append($("<td></td>").text(rowCount++));
-  
-  if (rowData.h) {
-    const $cell = $("<td></td>");
-    $("<img>").attr({
-      src: rowData.h,
-      style: "max-width:100px; max-height:100px;"
-    }).appendTo($cell);
-    $newRow.append($cell);
-  } else {
-    $newRow.append($("<td></td>"));
-  }
-    $newRow.append($("<td></td>").text(rowData.hh || ""));
-    $newRow.append($("<td></td>").text(rowData.jj || ""));
-    $newRow.append($("<td></td>").text(rowData.jgjgj || ""));
-  
-  $tableBody.append($newRow);
-}
+  function addRowToTable(rowData) {
+    const $tableBody = $("#dataTableBody");
+    const $newRow = $("<tr></tr>");
 
-  $('#date').on('change', function() {
-    const selectedDate = new Date($(this).val());
-    const today = new Date(); today.setHours(0,0,0,0);
-    let isValid = true;
-    let errorMessage = "";
-    
-    if (selectedDate >= today) {
-      isValid = false;
-      errorMessage = "ỷyery";
-    }
-    
-    if (!isValid) {
-      $(this).addClass('is-invalid').removeClass('is-valid');
-      $(this).siblings('.invalid-feedback').text(errorMessage).show();
+    $newRow.append($("<td></td>").text(rowCount++));
+
+    $newRow.append($("<td></td>").text(rowData.ma || ""));
+    if (rowData.mail) {
+      const $cell = $("<td></td>");
+      $("<img>")
+        .attr({
+          src: rowData.mail,
+          style: "max-width:100px; max-height:100px;",
+        })
+        .appendTo($cell);
+      $newRow.append($cell);
     } else {
-      $(this).removeClass('is-invalid').addClass('is-valid');
+      $newRow.append($("<td></td>"));
     }
-  });
+
+    $tableBody.append($newRow);
+  }
 });
