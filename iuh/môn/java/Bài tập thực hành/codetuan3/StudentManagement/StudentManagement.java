61fc25fd2e0947ee22fbeae80f0422f1366bfc15
Shima_Rin <lelong190110gmail.com>
2025-09-29 10:24:37 +0700

    Refactor student management system: remove CreateBinaryFile class, enhance Main and StudentManagement classes with improved menu and functionality

--- iuh/môn/java/Bài tập thực hành/codetuan3/StudentManagement/StudentManagement.java
+++ iuh/môn/java/Bài tập thực hành/codetuan3/StudentManagement/StudentManagement.java
@@ -1,4 +1,4 @@
-package StudentManagement;
+package codetuan3.StudentManagement;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -10,7 +10,7 @@ public class StudentManagement {
     private LinkedList<Student> studentList;
 
     @SuppressWarnings("unchecked")
-	public StudentManagement() {
+    public StudentManagement() {
         this.setStudentList(new LinkedList());
     }
 
@@ -18,44 +18,44 @@ public class StudentManagement {
         return studentList;
     }
 
-
     public void setStudentList(LinkedList<Student> studentList) {
         this.studentList = studentList;
     }
 
-	public void addStudent(Student student) {
-		this.studentList.add(student);
-	}
+    public void addStudent(Student student) {
+        this.studentList.add(student);
+    }
 
-	public void displayStudents() {
+    public void displayStudents() {
         System.out.println("--- DANH SÁCH SINH VIÊN ---");
         if (studentList.isEmpty()) {
             System.out.println("Danh sách trống.");
         } else {
-      
+
             for (Student student : studentList) {
-    
+
                 System.out.println(student);
             }
         }
         System.out.println("--------------------------");
     }
-	public void sortByName() {
-    
+
+    public void sortByName() {
+
         this.studentList.sort(Comparator.comparing(Student::getName));
         System.out.println("Đã sắp xếp danh sách theo tên.");
     }
-	public void sortBySe() {
-	    
+
+    public void sortBySe() {
+
         this.studentList.sort(Comparator.comparing(Student::getResult).reversed());
         System.out.println("Đã sắp xếp danh sách theo result từ cao xuống thấp.");
     }
 
-
     public void removeStudent(int id) {
 
         boolean isRemoved = this.studentList.removeIf(student -> student.getId() == id);
-        
+
         if (isRemoved) {
             System.out.println("Đã xóa sinh viên có ID: " + id);
         } else {
