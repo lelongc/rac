3b8a60eba29028fc120dbe19b2a3c04a5d60a56a
User <user@example.com>
2026-03-03 15:38:10 +0700

    kk

--- iuh/môn/hk2-3/ptht-tichhop/t5/Main.java
+++ iuh/môn/hk2-3/ptht-tichhop/t5/Main.java
@@ -1,8 +1,13 @@
+import java.net.URISyntaxException;
+
 public class Main {
-      public static void main(String[] args) throws InterruptedException {
+      public static void main(String[] args) throws InterruptedException, URISyntaxException {
+            String outputDir = new java.io.File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getPath();
+            
             FileThreadWriter[] t = new FileThreadWriter[3];
             for (int i = 0; i < 3; i++) {
-                  t[i] = new FileThreadWriter("file" + (i + 1) + ".txt", 10);
+                  String filePath = outputDir + java.io.File.separator + "file" + (i + 1) + ".txt";
+                  t[i] = new FileThreadWriter(filePath, 10);
                   t[i].setName("T" + (i + 1));
                   t[i].start();
             }
